7b29182797316425e62745e54e712cb153d4e6fe
Derek Burke <vagrant83@gmail.com>
2017-01-25 14:52:14 -0500

    /

--- challengesolution.py
+++ challengesolution.py
@@ -22,27 +22,19 @@ class NetworkDevice(object):
             self.remote_conn = telnetlib.Telnet(ip, TELNET_PORT, TELNET_TIMEOUT)
         except socket.timeout:
             sys.exit("Connection timed-out")
-    def login(remote_conn, username, password):
-        output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
-        remote_conn.write(username + '\n')
-        output += remote_conn.read_until("ssword:", TELNET_TIMEOUT)
-        remote_conn.write(password + '\n')
+    def login(self):
+        output = self.remote_conn.read_until("sername:", TELNET_TIMEOUT)
+        self.remote_conn.write(username + '\n')
+        output += self.remote_conn.read_until("ssword:", TELNET_TIMEOUT)
+        self.remote_conn.write(password + '\n')
         return output
-    def send_command(remote_conn, cmd):
+    def send_command(self, cmd):
         cmd = cmd.rstrip()
-        remote_conn.write(cmd + '\n')
+        self.remote_conn.write(cmd + '\n')
         time.sleep(1)
-        return remote_conn.read_very_eager()
-    def login(remote_conn, username, password):
-        output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
-        remote_conn.write(username + '\n')
-        output += remote_conn.read_until("ssword:", TELNET_TIMEOUT)
-        remote_conn.write(password + '\n')
-        return output
-    def disable_paging(remote_conn, paging_cmd='terminal length 0'):
-        return send_command(remote_conn, paging_cmd)
-    def close():
-        remote_conn.close()
+        return self.remote_conn.read_very_eager()
+    def close(self):
+        self.remote_conn.close()
 
 
 def main():
@@ -54,7 +46,7 @@ def main():
     output = rtr1.login()
     time.sleep(1)
     rtr1.read_very_eager()
-    rtr1.disable_paging()
+    output = rtr1.send_command('terminal length 0')
     output = rtr1.send_command('show ip int brief')
     print "\n\n"
     print output
