6f9b8a4e6e9d7fabf2f958bd9eee7b1a0c298a73
Derek Burke <vagrant83@gmail.com>
2017-01-27 14:54:20 -0500

    .

--- SNMP/snmpv3graph.py
+++ SNMP/snmpv3graph.py
@@ -9,30 +9,61 @@ auth_proto = 'sha'
 enc_key = 'galileo1'
 port = 161
 rtr1 = ('184.105.247.70', port)
-rtr2 = ('184.105.247.71', port)
 snmp_user = (user, auth_key, enc_key)
 oids = (
-	('sysName' = '1.3.6.1.2.1.1.5.0'),
+	('sysName', '1.3.6.1.2.1.1.5.0'),
 	('ifDescr_fa4', '1.3.6.1.2.1.2.2.1.2.5'),
 	('ifInOctets_fa4', '1.3.6.1.2.1.2.2.1.10.5'),
 	('ifInUcastPkts_fa4', '1.3.6.1.2.1.2.2.1.11.5'),
 	('ifOutOctets_fa4', '1.3.6.1.2.1.2.2.1.16.5'),
 	('ifOutUcastPkts_fa4', '1.3.6.1.2.1.2.2.1.17.5')
 )
-duration = 10
+duration = 12
+title = ''
+interface = ''
+ino = [0]
+inu = [0]
+outo = [0]
+outu = [0]
 
 #Function to retrieve SNMPv3 data at an OID
 def retrieveoidv3(rtr, user, oid, proto):
 	r = snmp_get_oid_v3(rtr, user, oid, proto)
 	info = snmp_extract(r)
-	print info + '\n'
+	return info
 
 #Execute this script if run directly
 if __name__ == '__main__':
 	while duration > 0:
 		for n, oid in oids:
-			retrieveoidv3(rtr1, snmp_user, oid, auth_proto)
-			retrieveoidv3(rtr2, snmp_user, oid, auth_proto)
-			print "%s %s" % (n, oid)
+			datapoint = retrieveoidv3(rtr1, snmp_user, oid, auth_proto)
+			if 'sysName' in n:
+				title = datapoint
+			if 'ifDescr' in n:
+				interface = datapoint
+			if 'InOctets' in n:
+				ino.append(int(datapoint) - ino[-1])
+			if 'InUcast' in n:
+				inu.append(int(datapoint) - inu[-1])
+			if 'OutOctets' in n:
+				outo.append(int(datapoint) - outo[-1])
+			if 'OutUcast' in n:
+				outu.append(int(datapoint) - outu[-1])
 		time.sleep(300)
 		duration -= 1
+
+	#Generate input/output octets line chart
+	chartoctets = pygal.Line()
+	chartoctets.title = title + interface + 'Octets'
+	chartoctets.x_labels = ['5', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55', '60']
+	chartoctets.add('InOctets', ino)
+	chartoctets.add('OutOctets', outo)
+	chartoctets.render_to_file('octets.svg')
+
+	#Generate input/output unicast packets line chart
+	chartunicast = pygal.Line()
+	chartunicast.title = title + interface + 'Unicast Packets'
+	chartunicast.x_labels = ['5', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55', '60']
+	chartunicast.add('InUcastPkts', inu)
+	chartunicast.add('OutUcastPkts', outu)
+	chartunicast.render_to_file('unicast.svg')
--- testing.py
+++ testing.py
@@ -5,10 +5,10 @@ oids = (
     ('ifOutOctets_fa4', '1.3.6.1.2.1.2.2.1.16.5'),
     ('ifOutUcastPkts_fa4', '1.3.6.1.2.1.2.2.1.17.5')
 )
-
-def test(saying, oid):
-    print "%s %s" % (saying, oid)
+ino = [1,2,3,4,5]
 
 for n, oid in oids:
-    test(n, oid)
-    test(n, oid)
+    if 'InOctets' in n:
+        print "%s %s" % (n, oid)
+
+print 11 - ino[-1]
