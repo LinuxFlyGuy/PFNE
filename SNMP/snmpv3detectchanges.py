c15ddff4ac95c852105ac1974c8f03ba4c012c4d
Derek Burke <vagrant83@gmail.com>
2017-01-31 10:57:19 -0500

    .

--- SNMP/snmpv3detectchanges.py
+++ SNMP/snmpv3detectchanges.py
@@ -37,6 +37,52 @@ def send_mail(receipient, subject, message, sender):
 	smtp_conn.quit()
 	return True
 
+def write_files(choice):
+	for n, oid in oids:
+		data = retrieveoidv3(rtr2, snmp_user, oid, auth_proto)
+		if choice == 1:
+			f = open('configchange.pkl', "wb")
+			pickle.dump(data, f)
+			f.close()
+		elif choice == 2:
+			with open('configchange.json', "w") as f:
+				json.dump(data, f)
+		elif choice == 3:
+			with open('configchange.yml', "w") as f:
+				yaml.dump(data, f)
+		else:
+			print "Something went horribly wrong"
+			exit()
+
+
+def compare_data(choice):
+	for n, oid in oids:
+		data = retrieveoidv3(rtr2, snmp_user, oid, auth_proto)
+		if choice == 1:
+			f = open('configchange.pkl', "rb")
+			a = pickle.load(f)
+			if data != a:
+				time = retrieveoidv3(rtr2, snmp_user, sysUptime, auth_proto)
+				message = "A configuration change was detected at", time
+				send_mail(to, subject, message, sender)
+		elif choice == 2:
+			with open('configchange.json') as f:
+				a = json.loads(f)
+			if data != a:
+				time = retrieveoidv3(rtr2, snmp_user, sysUptime, auth_proto)
+				message = "A configuration change was detected at", time
+				send_mail(to, subject, message, sender)
+		elif choice == 3:
+			with open('configchange.yml') as f:
+				a = yaml.loads(f)
+			if data != a:
+				time = retrieveoidv3(rtr2, snmp_user, sysUptime, auth_proto)
+				message = "A configuration change was detected at", time
+				send_mail(to, subject, message, sender)
+		else:
+			print "Something went horribly wrong"
+			exit()
+
 #Execute this if script is run directly
 if __name__ == '__main__':
 	print '''Save data in which format?
@@ -49,30 +95,7 @@ if __name__ == '__main__':
 	else:
 		print "That is not a valid choice; defaulting to Pickle"
 		choice = 1
+	write_files(choice)
 	while True:
-		for n, oid in oids:
-			data = retrieveoidv3(rtr2, snmp_user, oid, auth_proto)
-			if int(option) == 1:
-				f = open('configchange.pkl', "rb")
-				a = pickle.load(f)
-				if data != a:
-					time = retrieveoidv3(rtr2, snmp_user, sysUptime, auth_proto)
-					message = "A configuration change was detected at", time
-					send_mail(to, subject, message, sender)
-			elif int(option) == 2:
-				with open('configchange.json', "r") as f:
-					a = json.loads(f)
-				if data != a:
-					time = retrieveoidv3(rtr2, snmp_user, sysUptime, auth_proto)
-					message = "A configuration change was detected at", time
-					send_mail(to, subject, message, sender)
-			elif int(option) == 3:
-				with open('configchange.yml') as f:
-					a = yaml.loads(f)
-				if data != a:
-					time = retrieveoidv3(rtr2, snmp_user, sysUptime, auth_proto)
-					message = "A configuration change was detected at", time
-					send_mail(to, subject, message, sender)
-			else:
-				print "Something went horribly wrong"
-		time.sleep(60)
+		compare_data(choice)
+		time.sleep(15)
