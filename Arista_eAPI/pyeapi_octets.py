e7d7e47e89c8075aaa3f66d91231f34a919c9642
LinuxFlyGuy <vagrant83@gmail.com>
2017-03-02 18:25:14 -0800

    .

--- Arista_eAPI/pyeapi_octets.py
+++ Arista_eAPI/pyeapi_octets.py
@@ -6,6 +6,14 @@ from pprint import pprint
 pynet_sw3 = pyeapi.connect_to("pynet-sw3")
 
 shinterfaces = pynet_sw3.enable("show interfaces")
-
-for interface in shinterfaces:
-    pprint(interface)
+unlist = shinterfaces[0]
+interfaces = unlist['result']
+ethernets = []
+for interface in interfaces['interfaces']:
+	if 'Ethernet' in interface:
+		 ethernets.append(interface)
+for item in ethernets:
+	print item
+	print 'inOctets ' + str(interfaces['interfaces'][item]['interfaceCounters']['inOctets'])
+	print 'outOctets ' + str(interfaces['interfaces'][item]['interfaceCounters']['outOctets'])
+		
