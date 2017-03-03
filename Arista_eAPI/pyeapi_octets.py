1f104f417ddd950ce309db6cba21ec489ccaa3eb
Derek Burke <derek@localhost.localdomain>
2017-03-02 20:38:51 -0500

    .

---
+++ Arista_eAPI/pyeapi_octets.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import pyeapi
+from pprint import pprint
+
+pynet_sw3 = pyeapi.connect_to("pynet-sw3")
+
+shinterfaces = pynet_sw3.enable("show version")
+pprint(shinterface)
